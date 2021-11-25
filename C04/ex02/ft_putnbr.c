93c4a6fd99e78c388e09c48f1c386a7b35e08e7a
Patrick Cruz <pacruz@shi-r7-p5.s19.be>
2021-11-25 14:59:55 +0100

    Added C04

---
+++ C04/ex02/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pacruz <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 16:18:05 by pacruz            #+#    #+#             */
+/*   Updated: 2021/11/18 11:49:33 by pacruz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2137483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	else if (nb >= 0 && nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else
+	{
+		ft_putchar('-');
+		ft_putnbr(nb * -1);
+	}
+}
